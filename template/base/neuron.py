d0003c86366eab9c9611805f33b0f524b18fb02a
conidig <conidig@gmail.com>
2024-10-08 12:51:24 +0200

    Update neuron.py

--- template/base/neuron.py
+++ template/base/neuron.py
@@ -173,8 +173,7 @@ class BaseNeuron(ABC):
         )  # don't set weights if you're a miner
 
     def save_state(self):
-        bt.logging.warning(
-            "save_state() not implemented for this neuron. You can implement this function to save model checkpoints or other useful data."
+        
         )
 
     def load_state(self):
